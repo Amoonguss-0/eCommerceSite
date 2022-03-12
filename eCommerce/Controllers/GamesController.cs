dc0741cd91c9a3a809299fafdd9bed6f6e95d2e2
Justin-F5972 <justinfernandez27@outlook.com>
2022-03-11 19:07:49 -0800

    Had the create generated by Visual Studio

---
+++ eCommerce/Controllers/GamesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
