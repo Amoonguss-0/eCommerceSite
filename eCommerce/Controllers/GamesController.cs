7916b0d7436cb312e40d620f3daf07a689a49c69
Justin-F5972 <justinfernandez27@outlook.com>
2022-03-13 01:53:26 -0800

    Asynchronous code was added

--- eCommerce/Controllers/GamesController.cs
+++ eCommerce/Controllers/GamesController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly VideoGameContext _context;
+        public GamesController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                //
+
+                _context.Games.Add(game); // Prepares insert
+                await _context.SaveChangesAsync(); // Executes pending inset
+
+                ViewData["Message"] = $"{game.Title} was added successfully!"; // Show a success message on the page           
+                return View();
+            }
+            return View(game);
         }
     }
 }
