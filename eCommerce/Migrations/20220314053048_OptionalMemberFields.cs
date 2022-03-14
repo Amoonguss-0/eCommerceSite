5aa54ef5385623d203c610e15c8ed8f41cb35ce7
Justin-F5972 <justinfernandez27@outlook.com>
2022-03-13 22:39:20 -0700

    Made member phone and number optional and added HTTP Post

---
+++ eCommerce/Migrations/20220314053048_OptionalMemberFields.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerce.Migrations
+{
+    public partial class OptionalMemberFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Members_Members_MemberId1",
+                table: "Members");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Members_MemberId1",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "MemberId1",
+                table: "Members");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MemberId1",
+                table: "Members",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_MemberId1",
+                table: "Members",
+                column: "MemberId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Members_Members_MemberId1",
+                table: "Members",
+                column: "MemberId1",
+                principalTable: "Members",
+                principalColumn: "MemberId");
+        }
+    }
+}
--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -9,19 +9,16 @@ namespace eCommerce.Models
         public int MemberId { get; set; }
 
 
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
 
 
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
 
 
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
 
-        public string Username { get; set; }    
-
-
-        public DbSet<Member> Members { get; set; }
+        public string? Username { get; set; }    
     }
 
 
